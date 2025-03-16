73f31122b725039f6f2dbd6749316fddc62b4ddc
lua-code-hub <lua.code.hub@gmail.com>
2025-03-16 04:01:03 -0500

    Update Bye-UD-Aim.lua

--- Lua/Codes/Bye-UD-Aim.lua
+++ Lua/Codes/Bye-UD-Aim.lua
@@ -1,11 +1,29 @@
+-- Add this at the end of your existing code
 local function DestroyESP()
-    for _, esp in pairs(Settings.ESP.Players) do
+    -- Remove all ESP drawings
+    for player, esp in pairs(Settings.ESP.Players) do
         for _, drawing in pairs(esp) do
             drawing:Remove()
         end
+        Settings.ESP.Players[player] = nil
     end
-    Settings.ESP.Players = {}
+
+    -- Remove FOV Circle
     FOVCircle:Remove()
+
+    -- Cleanup connections
+    for _, connection in pairs({
+        Players.PlayerAdded,
+        Players.PlayerRemoving,
+        UserInputService.InputBegan,
+        RunService.RenderStepped
+    }) do
+        connection:Disconnect()
+    end
+
+    -- Clear settings
+    Settings = nil
 end
 
+-- Execute cleanup immediately
 DestroyESP()
